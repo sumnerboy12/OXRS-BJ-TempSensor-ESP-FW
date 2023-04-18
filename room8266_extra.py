745da5e47d94ecf5a50d24976ed49abf85497925
ben <ben.jones12@gmail.com>
2023-04-18 21:35:29 +1200

    fix room8266 build (hopefully)

--- room8266_extra.py
+++ room8266_extra.py
@@ -2,10 +2,9 @@ Import("env")
 
 env.AddPostAction(
     "$BUILD_DIR/${PROGNAME}.bin", 
-    "esptool.py --chip esp32 merge_bin -o $BUILD_DIR/${PROGNAME}_FLASHER.bin " + 
+    "esptool.py --chip esp8266 merge_bin -o $BUILD_DIR/${PROGNAME}_FLASHER.bin " + 
     " --flash_mode dio --flash_freq 40m --flash_size 4MB " +
     " 0x1000 $BUILD_DIR/bootloader.bin " +
     " 0x8000 $BUILD_DIR/partitions.bin " + 
-    " 0xe000 " + 
     " 0x10000 $BUILD_DIR/${PROGNAME}.bin"
 )
